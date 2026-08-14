57fdc02c9c01aa376664245fc4c1364d28548365
Yevheniia <evgeniakazak30@gmail.com>
2026-08-14 19:03:14 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+const btn = document.querySelector('button');
+btn.onclick = ()=> {
+    window.open('https://www.olx.ua/d/uk/obyavlenie/labidohromis-ellou-antsitrus-tsihlidy-malavi-tanganiki-amerikanskie-IDPX1e5.html?isPreviewActive=0&search_reason=search%7Corganic&sliderIndex=2');
+}
